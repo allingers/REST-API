93f021fbfc0a8bec34b75028033f2bd2c6889fc2
Amanda <aamandaallinger@outlook.com>
2023-01-05 10:27:30 +0100

    GET, POST and userWithId

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
-import usersRoutes from './routes/users';
-
+import usersRoutes from './routes/users.js'; 
 
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
 
-add.use ('/users', usersRoutes);
-
+app.use('/users', usersRoutes);
 
 app.get('/', (req, res) => res.send('Hello from homePage.'));
+
 
 
 app.listen (PORT, () => console.log(`Server är igång: http://localhost:${PORT}`));
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
+
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
+
 
 const router = express.Router();
 
+const users = [];
+
+
+//GET
 router.get('/', (req, res) => {
-    res.send('Hello world');
+
+    res.send(users);
+
+});
+
+//POST
+router.post('/', (req, res) => {
+    const user = req.body;
+
+    users.push({...user, id:  uuidv4() });
+
+    res.send(`user with the name ${user.firstName} added`); 
 });
 
 export default router;
